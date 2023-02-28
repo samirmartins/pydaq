0ac83941dc278340d81a9d0efa3b2d4796c5ff54
Samir Martins <milani.martins@gmail.com>
2023-02-28 09:45:42 -0300

    Updatable plot added plus few corrections

--- pydaqmx/step_response.py
+++ pydaqmx/step_response.py
@@ -3,8 +3,10 @@ import time
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os
+from timesched import Scheduler
+import numpy as np
 
-def step_response(device="AnalogInput", channel = "ai0", ts = 1.0, session_duration = 600, save = False, path = None):
+def step_response(device="AnalogInput", channel = "ai0", ts = 1.0, session_duration = 60.0, save = False, path = None):
     """
         This function can be used for data acquisition and step response experiments using Python + NIDAQmx boards.
 
@@ -23,23 +25,31 @@ def step_response(device="AnalogInput", channel = "ai0", ts = 1.0, session_durat
     :return:
 
     :example:
-        step_response("AnalogInput", "ai0", 1.0, 600, True, "C:\Users\Samir\Desktop")
-
+        step_response("AnalogInput", "ai0", 1.0, 60.0, True, "C:\\Users\\Samir\\Desktop")
     """
 
     # Initializing variables
     data = []
-    time = []
+    time_var = []
 
     # Number of cycles necessary
-    cycles = session_duration/ts
+    cycles = int(np.floor(session_duration/ts))
 
     # Initializing device, with channel defined
     task = nidaqmx.Task()
     task.ai_channels.add_ai_voltage_chan(device+'/'+channel)
 
-    # Create the figure and axes objects
+    # create the figure and axes objects
     fig, ax = plt.subplots()
+
+    # Run GUI event loop
+    plt.ion()
+
+    # Title and labels and plot creation
+    plt.title("Step Response", fontsize=20)
+    plt.xlabel("Time")
+    plt.ylabel("Voltage")
+    line, = ax.plot(time_var, data)
 
     # Main loop, where data will be acquired
     for k in range(cycles):
@@ -52,10 +62,19 @@ def step_response(device="AnalogInput", channel = "ai0", ts = 1.0, session_durat
 
         # Queue data in a list
         data.append(temp)
-        time.append(k)
+        time_var.append(k)
 
-        # Update Interface
+        # Update interface
+        # updating data values
+        line.set_xdata(time_var)
+        line.set_ydata(data)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        ax.set_xlim([0, 1.1*session_duration])
+        ax.set_ylim([-1.1*max(np.abs(data)), 1.1*max(np.abs(data))])
 
+    # Closing task
+    task.close()
 
     # Check if data will or not be saved, and save accordingly
     if save:
@@ -63,43 +82,14 @@ def step_response(device="AnalogInput", channel = "ai0", ts = 1.0, session_durat
         if path is None: # Saving in Desktop if it is not defined
             path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
 
-        file_time = open(path+'\\time.dat', 'w')
-        for t in time:
+        # Saving time_var
+        for t in time_var:
+            file_time = open(path + '\\time.dat', 'w')
             file_time.write(str(t) + "\n")
-        file_time.close()
+            file_time.close()
 
-        file_data = open(path+'\\data.dat', 'w')
+        # Saving data
         for d in data:
+            file_data = open(path + '\\data.dat', 'w')
             file_data.write(str(d) + "\n")
-        file_data.close()
-
-
-
-    ################ Melhorias: executar aquisição a cada ts segundos (planejado), e não com sleep. Com sleep não é tão preciso como com scheduler.
-
-    #
-    # from random import randint
-    #
-    # import matplotlib.pyplot as plt
-    # from matplotlib.animation import FuncAnimation
-    #
-    # # create empty lists for the x and y data
-    # x = []
-    # y = []
-    #
-    # # create the figure and axes objects
-    # fig, ax = plt.subplots()
-    #
-    # def animate(i):
-    #     pt = randint(1, 9)  # grab a random integer to be the next y-value in the animation
-    #     x.append(i)
-    #     y.append(pt)
-    #
-    #     ax.clear()
-    #     ax.plot(x, y)
-    #     ax.set_xlim([0, 20])
-    #     ax.set_ylim([0, 10])
-    #
-    # ani = FuncAnimation(fig, animate, frames=20, interval=500, repeat=False)
-    #
-    # plt.show()
+            file_data.close()
